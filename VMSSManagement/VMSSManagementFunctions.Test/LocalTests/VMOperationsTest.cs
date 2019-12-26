b37afd1c3b8d933f1cd8ff45f47fc9a7c1ab24c9
joe longo <joseph.longo@outlook.com>
2019-12-25 23:37:27 -0500

    Get Blob Uri

--- VMSSManagement/VMSSManagementFunctions.Test/LocalTests/VMOperationsTest.cs
+++ VMSSManagement/VMSSManagementFunctions.Test/LocalTests/VMOperationsTest.cs
@@ -10,6 +10,7 @@ using Microsoft.Azure.Management.Fluent;
 using System.Collections.Generic;
 using Microsoft.Azure.Management.Compute.Fluent;
 using Microsoft.Azure.Management.AppService.Fluent.Models;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
 
 //https://github.com/Azure-Samples/compute-dotnet-manage-virtual-machine-scale-sets/blob/master/Program.cs
 namespace Local.Test
@@ -19,20 +20,44 @@ namespace Local.Test
     public class VMOperationsTest: BaseLocalTest
     {
 
+
+        string sourceResourceGroupName = "VMSSManagementTarget";
+        string sourceVMName = "vmSource";
+        string targetResourceGroupName = "VMSSManagementTemp";
+        string targetVMName = System.Guid.NewGuid().ToString();
+        string location = "eastus";
+
         [TestMethod]
-        public async Task GetVM()
+        public async Task CloneVM()
         {
             IAzure azure = Login();
             ILogger logger = new TestLogger(TestContext);
 
-            string sourceResourceGroupName = "VMSSManagementTarget";
-            string sourceVMName = "vmSource";
-            string targetResourceGroupName = "VMSSManagementTemp";
-
             var operations = new VMSSOperations.VMOperations(azure,logger);
-            var snapShots = await operations.CreateSnapshotsAsync(sourceResourceGroupName, sourceVMName, targetResourceGroupName);
 
-            Assert.IsTrue(snapShots.Count > 0);
+
+            var vm = await operations.CloneVM(sourceResourceGroupName, sourceVMName, targetResourceGroupName, targetVMName, targetResourceGroupName);
+
+            Assert.IsTrue(vm != null);
+
+        }
+
+        [TestMethod]
+        public async Task GetUri()
+        {
+            IAzure azure = Login();
+            ILogger logger = new TestLogger(TestContext);
+
+            string vmssResourceGroupName = "VMSSManagement";
+            string location = "eastus";
+
+            var operations = new VMSSOperations.VMOperations(azure, logger);
+
+            var uri = await operations.GetBlobUri(vmssResourceGroupName, location);
+
+            Assert.IsFalse(string.IsNullOrEmpty(uri));
+
+            TestContext.WriteLine(uri);
 
         }
 
--- VMSSManagement/VMSSOperations/VMOperations.cs
+++ VMSSManagement/VMSSOperations/VMOperations.cs
@@ -5,6 +5,13 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Management.Network.Fluent;
+using Microsoft.Azure.Management.Network.Fluent.NetworkInterface.Definition;
+using Microsoft.Azure.Management.Storage.Fluent;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Reflection;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
 
 namespace VMSSOperations
 {
@@ -19,59 +26,236 @@ namespace VMSSOperations
             this.logger = logger;
         }
 
-        public async Task<List<ISnapshot>> CreateSnapshotsAsync(string sourceResourceGroupName, string sourceVMName, string targetResourceGroupName)
+        public async Task<IVirtualMachine> CloneVM(string sourceResourceGroupName, string sourceVMName, string targetResourceGroupName, string targetVMName, string tempResourceGroupName)
         {
+
             var vm = azure.VirtualMachines.GetByResourceGroup(sourceResourceGroupName, sourceVMName);
+            var tags = GetTags(vm);
 
-            Dictionary<string, string> tags = GetTags(vm);
+            var tempImageName = $"{targetVMName}_image_temp";
 
-            var targetVMName = System.Guid.NewGuid().ToString();
+            var tempImage = await azure.VirtualMachineCustomImages.Define(targetVMName)
+                .WithRegion(vm.Region)
+                .WithExistingResourceGroup(tempResourceGroupName)
+                .FromVirtualMachine(vm)
+                .WithTags(tags)
+                .CreateAsync();
 
-            ISnapshot shapshot;
+            var nicDefinitions = await CreateNICs(sourceResourceGroupName, sourceVMName, targetResourceGroupName, targetVMName, vm, tags);
 
-            var cnt = 1;
-            string targetDiskName;
+            var newVM = await azure.VirtualMachines.Define(targetVMName)
+                .WithRegion(vm.Region)
+                .WithExistingResourceGroup(targetResourceGroupName)
+                .WithNewPrimaryNetworkInterface(nicDefinitions[0])
+                .WithWindowsCustomImage(tempImage.Id)
+                .WithAdminUsername("")
+                .WithAdminPassword("")
+                .WithTags(tags)
+                .CreateAsync();
 
-            var snapshots = new List<ISnapshot>();
 
-            foreach (var disk in vm.DataDisks)
+            return newVM;
+        }
+        public async Task<string> GetBlobUri(string tempResourceGroupName, string location)
+        {
+
+
+            const string storeAccountPrefix = "vmssmgmtstorage";
+            const string containerName = "scripts";
+            const string blobName = "sysprep.ps1";
+
+            var resourceGroup = azure.ResourceGroups.GetByName(tempResourceGroupName);
+
+            var b = Encoding.ASCII.GetBytes(resourceGroup.Id);
+            var crc32 = new Crc32();
+            var suffix = crc32.Get(b).ToString();
+
+            var storageAccountName = $"{storeAccountPrefix}{suffix}".ToLower().Substring(0, 23);
+
+
+            logger.LogInformation($"Get StorageAccount from Location: {location}; ResourceGroupName: {tempResourceGroupName}; StorageAccount Name: {storageAccountName}...");
+
+            var storageAcount = await GetStorageAccount(tempResourceGroupName, location, blobName, containerName, storageAccountName);
+
+            logger.LogInformation($"Get Blob Uri from Container: {containerName}; Blob: {blobName}...");
+
+            var cloundStorageCredentials = new StorageCredentials(storageAccountName, storageAcount.GetKeys()[0].Value);
+            var cloudStorageAccount = new Microsoft.WindowsAzure.Storage.CloudStorageAccount(cloundStorageCredentials, true);
+
+            var blobClient = cloudStorageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference(containerName);
+
+            var blob = container.GetBlockBlobReference(blobName);
+
+            var policy = new SharedAccessBlobPolicy()
             {
-                targetDiskName = $"{targetVMName}_DataDisk_{cnt.ToString()}";
-                logger.LogInformation($"{targetDiskName}: ");
+                Permissions = SharedAccessBlobPermissions.Read,
+                SharedAccessStartTime = DateTime.Now.ToUniversalTime(),
+                SharedAccessExpiryTime = DateTime.Now.AddMinutes(30).ToUniversalTime()
+            };
 
-                shapshot = await azure.Snapshots.Define(targetDiskName)
-                                    .WithRegion(vm.Region)
-                                    .WithExistingResourceGroup(targetResourceGroupName)
-                                    .WithWindowsFromDisk(disk.Value.Inner.ManagedDisk.Id)
-                                    .WithTags(tags)
-                                    .WithIncremental(false)
-                                    .CreateAsync();
+            var sasToken = blob.GetSharedAccessSignature(policy);
+            var uri = blob.Uri + sasToken;
 
-                logger.LogInformation(shapshot.Id);
+            logger.LogInformation($"Uri: {uri}");
 
-                cnt++;
-
-                snapshots.Add(shapshot);
-            }
-
-            targetDiskName = $"{targetVMName}_OSDisk";
-            logger.LogInformation($"{targetDiskName}: ");
-
-            shapshot = await azure.Snapshots.Define(targetVMName)
-                                .WithRegion(vm.Region)
-                                .WithExistingResourceGroup(targetResourceGroupName)
-                                .WithWindowsFromDisk(vm.StorageProfile.OsDisk.ManagedDisk.Id)
-                                .WithTags(tags)
-                                .WithIncremental(false)
-                                .CreateAsync();
-
-            logger.LogInformation(shapshot.Id);
-            snapshots.Add(shapshot);
-
-
-            return snapshots;
+            return uri;
 
         }
+
+        private async Task<IStorageAccount> GetStorageAccount(string resourceGroupName, string location, string blobName, string containerName, string storageAccountName)
+        {
+            logger.LogInformation($"\tGetStorageAccount Begin...");
+
+            IStorageAccount storageAccount = null;
+
+            storageAccount = azure.StorageAccounts.GetByResourceGroup(resourceGroupName, storageAccountName);
+
+            if (storageAccount == null)
+            {
+
+                logger.LogInformation($"Storage Account not found...");
+                logger.LogInformation($"\tCreating Storage Account...");
+
+                storageAccount = await azure.StorageAccounts.Define(storageAccountName)
+                    .WithRegion(location)
+                    .WithExistingResourceGroup(resourceGroupName)
+                    .WithAccessFromAllNetworks()
+                    .WithBlobStorageAccountKind()
+                    .WithOnlyHttpsTraffic()
+                    .CreateAsync();
+
+                var cloundStorageCredentials = new StorageCredentials(storageAccountName, storageAccount.GetKeys()[0].Value);
+                var cloudStorageAccount = new Microsoft.WindowsAzure.Storage.CloudStorageAccount(cloundStorageCredentials, true);
+
+                var blobClient = cloudStorageAccount.CreateCloudBlobClient();
+                var container = blobClient.GetContainerReference(containerName);
+
+                logger.LogInformation($"\tCreating container...");
+
+                var result = await container.CreateIfNotExistsAsync();
+
+                var permission = await container.GetPermissionsAsync();
+                permission.PublicAccess = BlobContainerPublicAccessType.Off;
+                await container.SetPermissionsAsync(permission);
+
+                logger.LogInformation($"\tCreating blob...");
+                var blob = container.GetBlockBlobReference(blobName);
+
+                var assembly = Assembly.GetExecutingAssembly();
+                var resourceName = "VMSSOperations.SysPrep.ps1";
+
+                var content = assembly.GetManifestResourceStream(resourceName);
+
+                logger.LogInformation($"\tUploading blob...");
+                await blob.UploadFromStreamAsync(content);
+
+            }
+
+            logger.LogInformation($"\tGetStorageAccount Complete.");
+
+            return storageAccount;
+
+        }
+
+
+
+        //public async Task<List<IVirtualMachine>> CreateTempImage(string sourceResourceGroupName, string sourceVMName, string targetResourceGroupName, string targetVMName, IVirtualMachine vm, Dictionary<string, string> tags)
+        //{
+
+
+        //	//var nics = await CreateNICs(sourceResourceGroupName, sourceVMName, targetResourceGroupName, targetVMName, vm, tags);
+
+        //	//var vmDefinition = azure.VirtualMachines.Define(targetVMName)
+        //	//						.WithRegion(vm.Region)
+        //	//						.WithExistingResourceGroup(targetResourceGroupName)
+        //	//						.WithNewPrimaryNetworkInterface(nics[0])
+        //	//						.WithSpecializedOSDisk
+
+
+
+        //	return null;
+
+        //}
+
+        public async Task<List<IWithCreate>> CreateNICs(string sourceResourceGroupName, string sourceVMName, string targetResourceGroupName, string targetVMName, IVirtualMachine vm, Dictionary<string, string> tags)
+        {
+            var results = new List<IWithCreate>();
+
+            var cnt = 1;
+            string targetResourceName;
+
+
+            foreach (var nicId in vm.NetworkInterfaceIds)
+            {
+                targetResourceName = $"{targetVMName}_NIC_{cnt.ToString()}";
+
+                var nic = azure.NetworkInterfaces.GetById(nicId);
+                var nsg = azure.NetworkSecurityGroups.GetById(nic.NetworkSecurityGroupId);
+
+                var newNic = azure.NetworkInterfaces.Define(targetResourceName)
+                                    .WithRegion(vm.Region)
+                                    .WithExistingResourceGroup(targetResourceGroupName)
+                                    .WithExistingPrimaryNetwork(nic.PrimaryIPConfiguration.GetNetwork())
+                                    .WithSubnet(nic.PrimaryIPConfiguration.SubnetName)
+                                    .WithPrimaryPrivateIPAddressDynamic()
+                                    .WithExistingNetworkSecurityGroup(nsg)
+                                    .WithTags(tags);
+
+                results.Add(newNic);
+            }
+
+            return results;
+        }
+
+        //public async Task<List<ISnapshot>> CreateSnapshotsAsync(string sourceResourceGroupName, string sourceVMName, string targetResourceGroupName, string targetVMName, IVirtualMachine vm, Dictionary<string, string> tags)
+        //{
+
+        //	var results = new List<ISnapshot>();
+
+        //	var cnt = 1;
+        //	string targetResourceName;
+
+        //	ISnapshot shapshot;
+
+
+
+        //	targetResourceName = $"{targetVMName}_OSDisk";
+        //	logger.LogInformation($"{targetResourceName}: ");
+
+        //	shapshot = await azure.Snapshots.Define(targetVMName)
+        //						.WithRegion(vm.Region)
+        //						.WithExistingResourceGroup(targetResourceGroupName)
+        //						.WithWindowsFromDisk(vm.StorageProfile.OsDisk.ManagedDisk.Id)
+        //						.WithTags(tags)
+        //						.WithIncremental(false)
+        //						.CreateAsync();
+
+        //	logger.LogInformation(shapshot.Id);
+        //	results.Add(shapshot);
+
+        //	foreach (var disk in vm.DataDisks)
+        //	{
+        //		targetResourceName = $"{targetVMName}_DataDisk_{cnt.ToString()}";
+        //		logger.LogInformation($"{targetResourceName}: ");
+
+        //		shapshot = await azure.Snapshots.Define(targetResourceName)
+        //											.WithRegion(vm.Region)
+        //											.WithExistingResourceGroup(targetResourceGroupName)
+        //											.WithWindowsFromDisk(disk.Value.Inner.ManagedDisk.Id)
+        //											.WithTags(tags)
+        //											.WithIncremental(false)
+        //											.CreateAsync();
+        //		logger.LogInformation(shapshot.Id);
+
+        //		cnt++;
+
+        //		results.Add(shapshot);
+        //	}
+
+        //	return results;
+
+        //}
 
         private Dictionary<string, string> GetTags(IVirtualMachine vm)
         {
